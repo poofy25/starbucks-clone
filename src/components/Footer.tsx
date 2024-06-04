d6cdf9ea6de78376ef423cc74f70dacea3079aab
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-06-04 10:09:28 +0300

    testing image optimization

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,12 @@ import testImg from '/public/gradient.webp'
 export default function Footer() {
   return (
     <footer className="flex flex-col w-full mt-32 px-10 pt-8 text-black border-0 border-t border-solid border-black box-border">
-      <Image src={testImg} alt="testimg" width="100" height="100" />
+      <Image
+        src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp"
+        alt="testimg"
+        width="100"
+        height="100"
+      />
       <div className="w-full flex gap-32 justify-start">
         <div className="flex flex-col gap-4 text-[18px]">
           <h3 className="text-[22px] mb-2">About us</h3>

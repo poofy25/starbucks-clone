bcd37cce18e3011aa4a17917b9f302f7f4256bef
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-06-04 10:32:22 +0300

    testing image optimization

--- src/components/SplitSection.tsx
+++ src/components/SplitSection.tsx
@@ -18,7 +18,7 @@ export default function SplitSection({ blockData }: { blockData: any }) {
       />
       <Image
         className="h-full w-[50%] object-cover"
-        src={blockData.image.url}
+        src={`${blockData.image.url}?w=${400}&q=${75}`}
         alt="img"
         width="400"
         height="400"

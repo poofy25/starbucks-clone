83002dfacf877f2ebd5c34ad36f7a080aad6f618
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-06-06 12:30:46 +0300

    fixing client cache

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -9,10 +9,10 @@ export default async function HomePageContent({ pageData }: { pageData: any }) {
         if (block.blockType === 'splitSection') {
           return <SplitSection blockData={block} key={index} />
         }
-        if (block.blockType === 'imageSwipper') {
-          console.log('BLOCK : ', block)
-          return <ImageSwipper blockData={block} key={index} />
-        }
+        // if (block.blockType === 'imageSwipper') {
+        //   console.log('BLOCK : ', block)
+        //   return <ImageSwipper blockData={block} key={index} />
+        // }
       })}
     </section>
   )
